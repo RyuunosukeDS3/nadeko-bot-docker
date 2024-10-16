e044d45626ac73746b293d4fdf720f1be757f4c5
RyuunosukeDS3 <31015675+RyuunosukeDS3@users.noreply.github.com>
2024-10-16 09:52:07 -0300

    Update install-nadeko.sh

--- install-nadeko.sh
+++ install-nadeko.sh
@@ -7,7 +7,6 @@ root=$(pwd)
 cd nadekobot
 
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
-dotnet restore -f --no-cache
 dotnet publish -r $(echo ${TARGETPLATFORM} | sed 's/amd64/x64/' | sed 's/\//-/g') -c Release -o output/ src/NadekoBot/NadekoBot.csproj 
 
 cd "$root"
