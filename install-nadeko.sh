97befec016d3ab623d7d1812d6e2f520d99e5525
RyuunosukeDS3 <31015675+RyuunosukeDS3@users.noreply.github.com>
2024-10-16 10:50:07 -0300

    Update install-nadeko.sh

--- install-nadeko.sh
+++ install-nadeko.sh
@@ -5,7 +5,7 @@ root=$(pwd)
 mkdir nadekobot
 cd nadekobot
 wget https://gitlab.com/api/v4/projects/9321079/packages/generic/NadekoBot-build/5.1.14/5.1.14-$(echo ${TARGETPLATFORM} | sed 's/amd64/x64/' | sed 's/\//-/g')-build.tar
-tar --strip-components=1 -xvf 5.1.14-linux-$(echo ${TARGETPLATFORM} | sed 's/amd64/x64/' | sed 's/\//-/g')-build.tar;
+tar --strip-components=1 -xvf 5.1.14-$(echo ${TARGETPLATFORM} | sed 's/amd64/x64/' | sed 's/\//-/g')-build.tar;
 
 cd "$root"
 
