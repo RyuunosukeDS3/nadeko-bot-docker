bc52ce0168a7fb34ebb0663a7d20b6fcd51b794e
RyuunosukeDS3 <31015675+RyuunosukeDS3@users.noreply.github.com>
2024-10-15 23:51:11 -0300

    Update install-prereq.sh

--- install-prereq.sh
+++ install-prereq.sh
@@ -4,10 +4,10 @@ echo "Installing dotnet"
 apt-get update
 apt-get install wget -y
 wget "https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb" -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 apt-get update;
-apt-get install -y apt-transport-https && sudo apt-get update;
+apt-get install -y apt-transport-https && apt-get update;
 apt-get install -y dotnet-sdk-8.0;
 
 
